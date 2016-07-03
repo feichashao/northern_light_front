bbb71e5357d575beba8e31f4d0e9c6cc7fb9baf2
feichashao <feichashao@gmail.com>
2016-07-03 21:03:50 +0800

    make email handling asyn.

--- data/my-script.js
+++ data/my-script.js
@@ -1,21 +1,22 @@
 // Account number obj.
-ac = $(".dataCol.col02").filter( function() { return this.innerHTML.match(/^[0-9]+$/) })[0];
+var ac = $(".dataCol.col02").filter( function() { return this.innerHTML.match(/^[0-9]+$/) })[0];
 // Contact name obj
-ct = $("a[id^='lookup']")[3];
+var ct = $("a[id^='lookup']")[3];
 
 // Account number
-ac_num = ac.innerHTML;
+var ac_num = ac.innerHTML;
 
 // Contact name
-ct_name = ct.innerHTML;
+var ct_name = ct.innerHTML;
 
 // Contact lookup URL
-ct_url = ct.getAttribute('onfocus').match(/\/[0-9]+.*nocache=[0-9]+/)[0];
+var ct_url = ct.getAttribute('onfocus').match(/\/[0-9]+.*nocache=[0-9]+/)[0];
+
+// Display message
+var msg = $("<p>init messager</p>");
+msg.appendTo(ac);
 
 // Email address
-ct_content = $.get(ct_url);
+ct_content = $.get(ct_url).done( function() { msg = $("<p>got email address.</p>");  } );
 // email = ct_content.responseText.match(/mailto\:.*@.*\"\>/)[0].match(/[a-zA-Z0-9\.]*@[a-zA-Z0-9\.]*/)[0];
 
-// Append message under account number
-msg = $("<p>some messages</p>");
-msg.appendTo(ac);
