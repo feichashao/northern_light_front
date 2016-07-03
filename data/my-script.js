275e093eba6e021b07a15531b214bddc0a0a6d7c
feichashao <feichashao@gmail.com>
2016-07-03 21:12:59 +0800

    match email

--- data/my-script.js
+++ data/my-script.js
@@ -13,10 +13,14 @@ var ct_name = ct.innerHTML;
 var ct_url = ct.getAttribute('onfocus').match(/\/[0-9]+.*nocache=[0-9]+/)[0];
 
 // Display message
-var msg = $("<p>init messager</p>");
+var msg = $("<p>init message</p>");
 msg.appendTo(ac);
 
 // Email address
-ct_content = $.get(ct_url).done( function() { msg = $("<p>got email address.</p>");  } );
+ct_content = $.get(ct_url).done( function(data) { 
+    email = data.match(/mailto\:.*@.*\"\>/)[0].match(/[a-zA-Z0-9\.]*@[a-zA-Z0-9\.]*/)[0];
+    alert(email);
+    msg = $("<p>got email</p>");
+} );
 // email = ct_content.responseText.match(/mailto\:.*@.*\"\>/)[0].match(/[a-zA-Z0-9\.]*@[a-zA-Z0-9\.]*/)[0];
 
